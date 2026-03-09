d8282480854395a140d9740d0fba606ea8697d7d
Roman Onishchenko <romanindev@gmail.com>
2026-03-09 15:02:57 +0100

    branh updates

--- src/components/Brand/Brand.tsx
+++ src/components/Brand/Brand.tsx
@@ -1,18 +1,39 @@
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const Brand = () => {
   return (
-    <Typography
+    <Box
       component={RouterLink}
       to="/"
       sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 1,
         textDecoration: 'none',
         color: 'inherit',
-        fontWeight: 700,
       }}
     >
-      dfsync
-    </Typography>
+      <Box
+        component="img"
+        src="/assets/logo.svg"
+        alt="dfsync"
+        sx={{
+          width: 28,
+          height: 28,
+          borderRadius: '4px',
+          backgroundColor: '#ffffff',
+          padding: '2px',
+          border: '1px solid transparent',
+          borderColor: 'divider',
+        }}
+      />
+      <Typography
+        variant="h6"
+        sx={{ display: 'flex', textDecoration: 'none', color: 'inherit', fontWeight: 700 }}
+      >
+        dfsync
+      </Typography>
+    </Box>
   );
 };
