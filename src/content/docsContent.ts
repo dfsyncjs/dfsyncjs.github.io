bb94a52bd1e0b8700b5120b7d6f3317aef582008
Roman Onishchenko <romanindev@gmail.com>
2026-03-15 12:51:04 +0100

    add retry support

--- src/content/docsContent.ts
+++ src/content/docsContent.ts
@@ -5,6 +5,7 @@ export const docsContent = {
     'create-client': () => import('../../docs/v1/create-client.md?raw'),
     auth: () => import('../../docs/v1/auth.md?raw'),
     hooks: () => import('../../docs/v1/hooks.md?raw'),
+    retry: () => import('../../docs/v1/retry.md?raw'),
     errors: () => import('../../docs/v1/errors.md?raw'),
     examples: () => import('../../docs/v1/examples.md?raw'),
   },
