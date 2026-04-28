b0e36cb0ea7e87db838272269e637fd5ff9cf667
Roman Onishchenko <romanindev@gmail.com>
2026-04-28 13:18:31 +0200

    update social links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,22 +24,6 @@ export const Footer = () => {
 
           <Stack direction="row" spacing={2}>
             <Link
-              href="https://www.npmjs.com/package/@dfsync/client"
-              target="_blank"
-              rel="noreferrer"
-              underline="hover"
-              onClick={createTrackedLinkHandler({
-                params: {
-                  cta_name: 'npm',
-                  location: 'footer',
-                  label: 'npm',
-                  link_url: 'https://www.npmjs.com/package/@dfsync/client',
-                },
-              })}
-            >
-              npm
-            </Link>
-            <Link
               href="https://github.com/dfsyncjs/dfsync"
               target="_blank"
               rel="noreferrer"
@@ -54,6 +38,38 @@ export const Footer = () => {
               })}
             >
               GitHub
+            </Link>
+            <Link
+              href="https://www.linkedin.com/company/dfsync/"
+              target="_blank"
+              rel="noreferrer"
+              underline="hover"
+              onClick={createTrackedLinkHandler({
+                params: {
+                  cta_name: 'linkedIn',
+                  location: 'footer',
+                  label: 'LinkedIn',
+                  link_url: 'https://www.linkedin.com/company/dfsync/',
+                },
+              })}
+            >
+              LinkedIn
+            </Link>
+            <Link
+              href="https://x.com/dfsyncjs"
+              target="_blank"
+              rel="noreferrer"
+              underline="hover"
+              onClick={createTrackedLinkHandler({
+                params: {
+                  cta_name: 'x',
+                  location: 'footer',
+                  label: 'X (Twitter)',
+                  link_url: 'https://x.com/dfsyncjs',
+                },
+              })}
+            >
+              X (Twitter)
             </Link>
           </Stack>
         </Stack>
